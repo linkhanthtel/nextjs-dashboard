6f8956879f882a931f996229bb6a09be218ff680
Lin Khant Htel <linkhanthtel@icloud.com>
2023-12-07 23:00:41 +0800

    Update
    Updated Commit

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardSkeleton from '../../ui/skeletons'
+
+function Loading() {
+  return (
+    <div>
+      <DashboardSkeleton />
+    </div>
+  )
+}
+
+export default Loading
